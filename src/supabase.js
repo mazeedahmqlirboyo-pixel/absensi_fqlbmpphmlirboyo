ad6e5c95939b8761bb758bc55b6049006099a2f3
mazeedahmqlirboyo-pixel <mazeedahmqlirboyo@gmail.com>
2026-04-11 22:32:12 +0700

    Hardcode supabase env variables for easy vercel deployment

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://psmswngqyzrnhkxwexkw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzbXN3bmdxeXpybmhreHdleGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NzU2NzcsImV4cCI6MjA5MTQ1MTY3N30.5TEhgcMrnPRQYwH_aambYIzBibonCsy2gmS_L6lOxgU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
